c32a6133dce8b2727e6e7f78560e9589a5bc2e5f
furasshujipushii <enzogireauds@gmail.com>
2023-07-26 21:47:04 +0200

    adding loader and lazy attribute to imgs

--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,45 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el))
+
+document.addEventListener("DOMContentLoaded", () => {
+  // La page HTML est chargée, commencez le chargement des autres ressources ici
+  let progress = 0;
+  const loaderProgress = document.querySelector(".loader-progress") as HTMLSpanElement;
+  const contents = document.querySelectorAll(".content") as NodeListOf<HTMLDivElement>;
+
+  function updateLoader() {
+    // Mettez à jour le chiffre pour afficher le pourcentage de chargement
+    loaderProgress.textContent = `${progress}%`;
+  }
+
+  function showContents() {
+    // Affiche tous les éléments avec la classe ".content"
+    contents.forEach((content) => {
+      content.style.display = "block";
+    });
+  }
+
+  function incrementLoader() {
+    // Incrémentez la valeur de "progress" jusqu'à atteindre 100
+    if (progress < 100) {
+      progress += 1; // Augmentez la valeur pour contrôler la vitesse du loader
+      updateLoader();
+      setTimeout(incrementLoader, 30); // Délai avant d'augmenter la valeur suivante (plus petit pour une meilleure animation)
+    } else {
+      // Une fois le chargement terminé, affichez tous les éléments avec la classe ".content"
+      showContents();
+
+      // Masquez le loader
+      const loader = document.querySelector(".loader") as HTMLDivElement;
+      if (loader) {
+        loader.style.display = "none";
+      }
+    }
+  }
+
+  incrementLoader();
+});
+
+
+

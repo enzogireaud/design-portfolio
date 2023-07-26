61e6cdbb81296f0de50b2d1b2507a7d768c0d69f
furasshujipushii <enzogireauds@gmail.com>
2023-07-26 11:52:55 +0200

    adding scroll anim

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 const btnOpen = document.getElementById("hamburger")
 const btnClose = document.getElementById("hamburger-close")
+const menuLinks = document.querySelectorAll(".menu-links")
 
 btnOpen?.addEventListener("click",() => {
   document.getElementById("menu")?.classList.add("active")
@@ -8,3 +9,21 @@ btnClose?.addEventListener("click",() => {
   document.getElementById("menu")?.classList.remove("active")
 })
 
+menuLinks.forEach((link) => {
+  link.addEventListener("click",() => {
+    document.getElementById("menu")?.classList.remove("active")
+  })
+})
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show")
+    } else {
+      entry.target.classList.remove("show")
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el) => observer.observe(el))

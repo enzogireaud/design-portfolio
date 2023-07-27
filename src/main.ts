961d324947631e126d5ae79472509bcd8e34c193
furasshujipushii <enzogireauds@gmail.com>
2023-07-27 09:09:50 +0200

    updating slider and new script for the slider

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,52 @@
-const btnOpen = document.getElementById("hamburger")
-const btnClose = document.getElementById("hamburger-close")
-const menuLinks = document.querySelectorAll(".menu-links")
+/*****
+ *
+ * Menu + Fade animation
+ *
+ ****/
+const btnOpen = document.getElementById("hamburger");
+const btnClose = document.getElementById("hamburger-close");
+const menuLinks = document.querySelectorAll(".menu-links");
 
-btnOpen?.addEventListener("click",() => {
-  document.getElementById("menu")?.classList.add("active")
-})
-btnClose?.addEventListener("click",() => {
-  document.getElementById("menu")?.classList.remove("active")
-})
+btnOpen?.addEventListener("click", () => {
+  document.getElementById("menu")?.classList.add("active");
+});
+btnClose?.addEventListener("click", () => {
+  document.getElementById("menu")?.classList.remove("active");
+});
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click",() => {
-    document.getElementById("menu")?.classList.remove("active")
-  })
-})
+  link.addEventListener("click", () => {
+    document.getElementById("menu")?.classList.remove("active");
+  });
+});
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if(entry.isIntersecting){
-      entry.target.classList.add("show")
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
     } else {
-      entry.target.classList.remove("show")
+      entry.target.classList.remove("show");
     }
-  })
-})
+  });
+});
 
-const hiddenElements = document.querySelectorAll(".hidden")
-hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
+/*****
+ *
+ * Loader
+ *
+ ****/
 document.addEventListener("DOMContentLoaded", () => {
   // La page HTML est chargée, commencez le chargement des autres ressources ici
   let progress = 0;
-  const loaderProgress = document.querySelector(".loader-progress") as HTMLSpanElement;
-  const contents = document.querySelectorAll(".content") as NodeListOf<HTMLDivElement>;
+  const loaderProgress = document.querySelector(
+    ".loader-progress"
+  ) as HTMLSpanElement;
+  const contents = document.querySelectorAll(
+    ".content"
+  ) as NodeListOf<HTMLDivElement>;
 
   function updateLoader() {
     // Mettez à jour le chiffre pour afficher le pourcentage de chargement
@@ -51,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (progress < 100) {
       progress += 1; // Augmentez la valeur pour contrôler la vitesse du loader
       updateLoader();
-      setTimeout(incrementLoader, 30); // Délai avant d'augmenter la valeur suivante (plus petit pour une meilleure animation)
+      setTimeout(incrementLoader, 20); // Délai avant d'augmenter la valeur suivante (plus petit pour une meilleure animation)
     } else {
       // Une fois le chargement terminé, affichez tous les éléments avec la classe ".content"
       showContents();
@@ -66,6 +80,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   incrementLoader();
 });
+
+
 
 
 
